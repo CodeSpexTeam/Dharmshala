41e3a22e35ba60f6a4ae616f18820e837e947adf
MG <immukesh@live.com>
2024-02-06 18:25:46 +0530

    added all admin components

--- dharmshala/src/app/app-routing.module.ts
+++ dharmshala/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './admin/signin/signin/signin.component';
+import { DashboardComponent } from './admin/dashboard/dashboard/dashboard.component';
+import { AboutComponent } from './admin/about/about/about.component';
+import { MemberComponent } from './admin/member/member/member.component';
+import { ProjectsComponent } from './admin/project/projects/projects.component';
+import { GalleryComponent } from './admin/gallery/gallery/gallery.component';
+import { SocialmediaComponent } from './admin/social media/socialmedia/socialmedia.component';
+import { FacilityComponent } from './admin/facility/facility/facility.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SigninComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'member', component: MemberComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'social-media', component: SocialmediaComponent },
+  { path: 'facility', component: FacilityComponent },
+  { path: '**', component: SigninComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dharmshala/src/app/app.module.ts
+++ dharmshala/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './admin/navbar/nav/nav.component';
+import { SigninComponent } from './admin/signin/signin/signin.component';
+import { DashboardComponent } from './admin/dashboard/dashboard/dashboard.component';
+import { AboutComponent } from './admin/about/about/about.component';
+import { MemberComponent } from './admin/member/member/member.component';
+import { ProjectsComponent } from './admin/project/projects/projects.component';
+import { GalleryComponent } from './admin/gallery/gallery/gallery.component';
+import { SocialmediaComponent } from './admin/social media/socialmedia/socialmedia.component';
+import { FacilityComponent } from './admin/facility/facility/facility.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SigninComponent,
+    DashboardComponent,
+    AboutComponent,
+    MemberComponent,
+    ProjectsComponent,
+    GalleryComponent,
+    SocialmediaComponent,
+    FacilityComponent
   ],
   imports: [
     BrowserModule,
